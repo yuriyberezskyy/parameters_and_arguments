8082204fd5a3be36aaf6bee75a16182670e8c618
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-17 17:19:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet(first_name,last_name):
+  print(f"Hello {first_name} {last_name}")
+
+
+greet("Yuriy","Berezskyy")
+
+greet(last_name = "Smith", first_name = "Will")
